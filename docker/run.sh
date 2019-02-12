de1c4e5cdf3502448ebb9e559771e566b5d08055
Gary Sieling <gary@garysieling.com>
2019-02-12 15:08:51 -0500

    Move zeppelin environment settings out of the docker image

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,8 @@ docker run -p 7077:7077 \
   -v $PWD/logs:/logs \
   -v $PWD/notebook:/notebook \
   -v /data:/data \
+  -v $PWD/conf:/zeppelin/conf/ \
+  -it \
   -e ZEPPELIN_NOTEBOOK_DIR='/notebook' \
   -e ZEPPELIN_LOG_DIR='/logs' \
   apache/zeppelin:0.8.1
