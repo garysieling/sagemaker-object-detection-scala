048b046e49b277492a2fecbe596c3f4e90856a80
Gary Sieling <gsieling@us.imshealth.com>
2019-02-13 14:40:56 -0500

    Add a notebook that can connect to IFTTT

--- docker/run.sh
+++ docker/run.sh
@@ -1,12 +1,17 @@
+if [ -z ${DATA_DIR+x} ]; then 
+  DATA_DIR=/data
+fi
+
 docker run -p 7077:7077 \
   -p 9080:8080 \
   --privileged=true \
   -v $PWD/logs:/logs \
   -v $PWD/notebook:/notebook \
-  -v /data:/data \
+  -v $DATA_DIR:/data \
   -v $PWD/conf:/zeppelin/conf/ \
   -it \
   -e ZEPPELIN_NOTEBOOK_DIR='/notebook' \
   -e ZEPPELIN_LOG_DIR='/logs' \
+  -e IFTTT_TOKEN=$IFTTT_TOKEN \
   apache/zeppelin:0.8.1
 
